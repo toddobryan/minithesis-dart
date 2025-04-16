d0bd50f1a9fdc69db6ff1480677aa7b67fb21813
Todd O'Bryan <toddobryan@gmail.com>
2025-04-16 06:31:38 -0400

    modifying Twister

--- lib/minithesis.dart
+++ lib/minithesis.dart
@@ -4,9 +4,19 @@ import 'dart:math';
 import 'dart:typed_data';
 import 'package:collection/collection.dart';
 import 'package:crypto/crypto.dart';
+import 'package:fixnum/fixnum.dart';
 import 'package:path/path.dart' as p;
 
 import 'src/better_random.dart';
+
+class NamedFunction<F extends Function> {
+  NamedFunction(this.name, this.f);
+
+  final String name;
+  final F f;
+
+  F get call => f;
+}
 
 // Dart uses 64 bit ints on native, but only 32 bits are reliable on web
 final int maxBitLength = 63;
@@ -27,10 +37,10 @@ void Function(void Function(String, TestCase)) runTest([
   Database? database,
   bool quiet = false,
 ]) {
-  void accept(String name, void Function(TestCase) test) {
+  void accept(NamedFunction<void Function(TestCase)> test) {
     void markFailuresInteresting(TestCase testCase) {
       try {
-        test(testCase);
+        test.call(testCase);
       } catch (e) {
         if (testCase.status != null) {
           rethrow;
@@ -47,10 +57,16 @@ void Function(void Function(String, TestCase)) runTest([
 
     Database db = database ?? DirectoryDb.fromPath(".minithesis-cache");
 
-    var previousFailure = db[name];
+    var previousFailure = db[test.name];
 
     if (previousFailure != null) {
-      choices
+      int l = previousFailure.length;
+      List<int> theIs = List<int>.generate(l ~/ 8, (x) => x * 8);
+      List<Int64> choices = [
+        for (int i in theIs)
+          Int64.fromBytes(previousFailure.sublist(i, min(l, i + 8))),
+      ];
+      state.testFunction(TestCase.forChoices(choices));
     }
   }
 }
@@ -63,15 +79,18 @@ class TestCase {
     this.printResults = false,
   ]);
 
-  factory TestCase.forChoices(List<int> choices, [bool printResults = false]) {
+  factory TestCase.forChoices(
+    List<Int64> choices, [
+    bool printResults = false,
+  ]) {
     return TestCase(choices, null, choices.length, printResults);
   }
 
-  Iterable<int> prefix;
+  Iterable<Int64> prefix;
   BetterRandom? random;
   num maxSize = double.infinity;
   bool printResults = false;
-  List<int> choices = List<int>.empty();
+  List<Int64> choices = List<Int64>.empty();
   Status? status;
   int depth = 0;
   int? targetingScore;
@@ -146,8 +165,8 @@ class TestCase {
 
   bool _shouldPrint() => printResults && depth == 0;
 
-  int _makeChoice(int n, int Function() rndMethod) {
-    late int result;
+  int _makeChoice(int n, Int64 Function() rndMethod) {
+    late Int64 result;
     if (n < 0 || n.bitLength >= maxBitLength) {
       throw ArgumentError('Invalid choice $n');
     } else if (status != null) {
--- lib/src/better_random.dart
+++ lib/src/better_random.dart
@@ -4,19 +4,23 @@ import 'dart:typed_data';
 import 'package:fixnum/fixnum.dart';
 
 class BetterRandom {
-  static final int minInt = -0x80000000;
-  static final int maxInt = 0x7FFFFFFF;
-  static final Int64 maxIntPlusOne = Int64(0x80000000);
+  static final int minInt32 = -0x80000000;
+  static final int maxInt32 = 0x7FFFFFFF;
+  static final Int64 minInt64 = Int64.fromInts(-0x80000000, 0);
+  static final Int64 maxInt64 = Int64.fromInts(0x7FFFFFFF, 0xFFFFFFFF);
+  static final int bitsInInt64 = 64;
+  static final int precision = 53;
 
   BetterRandom(this.seed);
 
-  factory BetterRandom.usingClock() => BetterRandom(DateTime.now().millisecondsSinceEpoch);
+  factory BetterRandom.usingClock() =>
+      BetterRandom(Int64(DateTime.now().microsecondsSinceEpoch));
 
-  final int seed;
+  final Int64 seed;
 
   _MersenneTwister? __twister;
 
-  _MersenneTwister get _twister  {
+  _MersenneTwister get _twister {
     if (__twister != null) {
       return __twister!;
     } else {
@@ -25,23 +29,24 @@ class BetterRandom {
     }
   }
 
-  /// Returns a random int where the first (32 - bits) bits are zero
-  /// and the rest are either 0 or 1 (mostly) at random.
-  int next(int bits) {
-    if (bits == 32) {
-      return nextInt();
-    } else if (bits <= 0 || bits > 32) {
-      throw ArgumentError('bits must be in range [1, 31], given: $bits');
+  /// Returns a random Int64 where the first (64 - bits) bits are zero
+  /// and the rest are either 0 or 1 at pseudo-random.
+  Int64 next(int bits) {
+    if (bits == bitsInInt64) {
+      return nextInt64();
+    } else if (bits <= 0 || bits > bitsInInt64) {
+      throw ArgumentError(
+        'bits must be in range [1, $bitsInInt64], given: $bits',
+      );
     } else {
-      return ((1 << bits) - 1) & nextInt();
+      return Int64((1 << bits) - 1) & nextInt();
     }
   }
 
-
-  /// Returns a pseudo-random int 
+  /// Returns a pseudo-random int
   /// in the range -2^31 to 2^31-1 inclusive.
   int nextInt() {
-    return _twister.nextInt();
+    return _twister.nextInt64() & minInt32;
   }
 
   /// Returns a pseudo-random int in the range [min, max),
@@ -59,7 +64,6 @@ class BetterRandom {
     } else {
       return _nextIntInUniformRange(bound) + min;
     }
-    
   }
 
   int nextNonNegIntBelow(int bound) {
@@ -70,47 +74,49 @@ class BetterRandom {
     }
 
     int m = bound - 1;
-    if ((bound & m) == 0) { // only true if bound is a power of 2
+    if ((bound & m) == 0) {
+      // only true if bound is a power of 2
       return nextInt() & m; // equivalent to nextInt() % bound
     } else {
       return _nextIntInUniformRange(Int64(bound)) % bound;
     }
   }
 
-  /// returns a pseudo-random int such that bound % n is equally likely for
-  /// n in range [1, bound - 1].
-  int _nextIntInUniformRange(Int64 bound) {
-    if (bound <= maxInt) { // we can fit at least two ranges for n % bound in 32-bit ints
-      int b = bound.toInt();
+  /// returns a pseudo-random Int64 such that max % n is equally likely for
+  /// n in range [0, max].
+  Int64 _nextIntInUniformRange(Int64 max) {
+    if (max < maxInt64) {
+      // we can fit at least two ranges for n % (max + 1) in Int64s
       // eliminate values at each end of the int range where remainders would be
-      // over-represented, i.e., we limit ourselves to a range where each 
+      // over-represented, i.e., we limit ourselves to a range where each
       // remainder occurs the same number of times, namely floor(2^32 / bound)
-      int leastMultipleOfBoundAboveMinInt = minInt + minInt.remainder(b);
-      int maxMultipleOfBoundBelowMaxIntMinusOne = maxInt - (maxInt % b) - 1;
-      int r = nextInt();
+      Int64 leastMultipleOfBoundAboveMinInt =
+          minInt64 + minInt64.remainder(max);
+      Int64 maxMultipleOfBoundBelowMaxIntMinusOne =
+          maxInt64 - (maxInt64 % max) - 1;
+      Int64 r = nextInt64();
       // the worst case for this is 2^30, where only half the values are in the acceptable range
       // so the expected number of times the while loop should run is <= 2
-      while (r < leastMultipleOfBoundAboveMinInt || r > maxMultipleOfBoundBelowMaxIntMinusOne) {
-        r = nextInt();
+      while (r < leastMultipleOfBoundAboveMinInt ||
+          r > maxMultipleOfBoundBelowMaxIntMinusOne) {
+        r = nextInt64();
       }
       return r;
     } else {
       // there's only room for one range where each n % bound is equally likely
       // we arbitrarily choose the range centered around zero
-      int negBound = -(bound >> 1).toInt();
-      int posBound = ((bound >> 1) - bound % 2).toInt();
-      int r = nextInt();
+      Int64 negBound = -(max >> 1);
+      Int64 posBound = ((max >> 1) - max % 2);
+      Int64 r = nextInt64();
       while (r < negBound || r > posBound) {
-        r = nextInt();
+        r = nextInt64();
       }
       return r;
     }
   }
 
   Int64 nextInt64() {
-    int top = _twister.nextInt();
-    int bottom = _twister.nextInt();
-    return Int64.fromInts(top, bottom);
+    return _twister.nextInt64();
   }
 
   double nextDouble() {
@@ -131,7 +137,6 @@ extension Int64RightShift on Int64 {
       }
       return num;
     }
-
   }
 }
 
@@ -143,25 +148,26 @@ class _MersenneTwister {
   static final int lowerMask = 0x7FFFFFFF;
 
   _MersenneTwister(this.seed) {
-    List<int> temp = [seed];
+    List<Int64> temp = [seed];
     for (int i = 1; i < n; i++) {
-      int nextVal = 1812433253 * (temp[i - 1] ^ (temp[i - 1] >>> 30)) + i;
+      Int64 prevTemp = temp[i - 1];
+      Int64 nextVal = Int64(1812433253) * prevTemp ^ (prevTemp >>> 30) + i;
       temp.add(nextVal);
     }
-    _mt = Int32List.fromList(temp);
+    // the toInt in the next line may have trouble in JS, but
+    _mt = [...temp];
   }
 
-  final int seed;
+  final Int64 seed;
   int _index = 0;
-  late Int32List _mt;
+  late List<Int64> _mt;
 
-
-  int nextInt() {
+  int nextInt64() {
     if (_index == 0) {
       _generateNumbers();
     }
 
-    Int32 y = Int32(_mt[_index]);
+    Int64 y = _mt[_index];
     y = y ^ (y >> 11);
     y = y ^ ((y << 7) & 0x9D2C5680);
     y = y ^ ((y << 15) & 0xEFC60000);
